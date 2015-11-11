d55a771baa8ff350270ffed20860d2b63352879d
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-11-11 16:24:29 -0300

    Better spec using integer instead of number.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- etude-4-3/powers.erl
+++ etude-4-3/powers.erl
@@ -3,16 +3,12 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--spec(raise(number(), number()) -> number()).
+-spec(raise(number(), integer()) -> number()).
 
-raise(_X, 0)	->
-	1;
-raise(X, 1) ->
-	X;
-raise(X, N) when N > 1 ->
-	X * raise(X, N-1);
-raise(X, N) when N < 0 ->
-	1.0 / raise(X, -N).
+raise(_X, 0) -> 1;
+raise(X, 1) -> X;
+raise(X, N) when N > 1 -> X * raise(X, N-1);
+raise(X, N) when N < 0 -> 1.0 / raise(X, -N).
 
 raise_test() ->
 	?assert(raise(5, 1) =:= 5),
