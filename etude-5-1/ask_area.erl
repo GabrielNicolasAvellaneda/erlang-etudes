fc34ef17e1abf987e08b2dd72b9935bb085d46d5
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-11-11 21:17:13 -0300

    Minor fixes in error handling.

--- etude-5-1/ask_area.erl
+++ etude-5-1/ask_area.erl
@@ -1,6 +1,7 @@
 -module(ask_area).
 -export([area/0]).
 
+-spec(char_to_shape(char())->atom()).
 char_to_shape(Char) ->
 	case Char of
 		$R -> rectangle;
@@ -12,6 +13,7 @@ char_to_shape(Char) ->
 		_ -> unknown
 	end.
 
+-spec(get_number(string()) -> number()).
 get_number(NumberString) ->
 	{Result, _} = string:to_float(NumberString),
 	case Result of	
@@ -21,14 +23,16 @@ get_number(NumberString) ->
 		_ -> Result
 	end.
 
+%% get_dimensions returns a number if it can parse a number. Or an atom if there is an error
+-spec(get_dimensions(string(), string()) -> {number(), number()}).
 get_dimensions(PromptA, PromptB) ->
 	A = io:get_line("Enter " ++  PromptA ++ " > "),
 	B = io:get_line("Enter " ++ PromptB ++ " > "),
 	{get_number(A), get_number(B)}.
 
 calculate(unknown, _, Error) -> io:format("~s~n", [Error]);
-calculate(_, {error, _}, _) -> io:format("Error in first number.~n");
-calculate(_, _, {error,_}) -> io:format("Error in second number.~n");
+calculate(_, error, _) -> io:format("Error in first number.~n");
+calculate(_, _, error) -> io:format("Error in second number.~n");
 calculate(_, A, B) when A =< 0; B =< 0 -> io:format("Both number must be greater than zero.");
 calculate(Shape, A, B) -> geom:area(Shape, A, B).
 
