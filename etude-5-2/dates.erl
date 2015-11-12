49bfad5a291a9432308776c823947b20bfba4f8f
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-11-11 23:42:17 -0300

    Using re:split options and element/2 for better readbility.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- etude-5-2/dates.erl
+++ etude-5-2/dates.erl
@@ -6,7 +6,7 @@
 
 -spec date_parts(string()) -> [number()].
 date_parts(DateString) ->
-	lists:map(fun (X) -> {N, _} = string:to_integer(binary:bin_to_list(X)), N end, re:split(DateString, "-")).
+	lists:map(fun (X) -> element(1, string:to_integer(X)) end, re:split(DateString, "-", [{return, list}])).
 
 date_parts_test() ->
 		?assert(date_parts("2015-11-10") =:= [2015, 11, 10]).
